870e54d775911350258ae98ce2535445d18309e1
Atheesh Kumar <73014127+athee490@users.noreply.github.com>
2023-10-31 18:28:52 +0530

    Add files via upload

---
+++ IOT-WEBSITE/iot/urls.py
@@ -0,0 +1,26 @@
+"""iot URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from myiot.views import iot,receive_json_data,send_json_data
+from django.urls import path
+from django.contrib import admin
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',iot, name='iot'),
+    path('receive_json_data/',receive_json_data,name='receive_json_data' ),
+    path('send_json_data/',send_json_data,name='send_json_data' ),
+]
